7fae6d8fffae13b787b20aa1c19fddca96b9f72d
kalebentley <kale.bentley@gmail.com>
2021-01-20 17:29:26 -0800

    updated creel model LUT

--- source files/Load_LUTs.R
+++ source files/Load_LUTs.R
@@ -3,4 +3,4 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     effort_xwalk<-read.csv(paste0(wd_LUTs, "/02_Crosswalk_Table_for_Index_TieIn_Sections_2019-01-10.csv"), header=TRUE) #Effort Section table
     river_loc<-read.csv(paste0(wd_LUTs, "/02_River.Locations_2019-01-07.csv"), header=TRUE) #River Location table
-    creel_models<-read.csv(paste0(wd_LUTs, "/02_Creel_Models_2019-04-12.csv"), header=TRUE) #Creel model file names and descriptions
+    creel_models<-read.csv(paste0(wd_LUTs, "/02_Creel_Models_2021-01-20.csv"), header=TRUE) #Creel model file names and descriptions
