fe3341e77be0d840e7fec9fe274ec854f7d6ec0b
tbuehrens <tbuehrens@gmail.com>
2021-01-28 17:31:52 -0800

    tweaks to RMD, add new load packages func.

---
+++ functions/install_or_load_pack.r
@@ -0,0 +1,6 @@
+install_or_load_pack <- function(pack){
+  create.pkg <- pack[!(pack %in% installed.packages()[, "Package"])]
+  if (length(create.pkg))
+    install.packages(create.pkg, dependencies = TRUE)
+  sapply(pack, require, character.only = TRUE)
+}
